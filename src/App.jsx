7b47cf673907a4e7884af54e6b59987c8c025fc0
Sritam Behera <bsritam227@gmail.com>
2023-12-10 22:16:08 +0530

    pages and route created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/Signin";
+import SignUp from "./Pages/SignUp";
+import About from "/.Pages/About";
+import Profile from "./Pages/Profile";
 
 export const App = () => {
-  return <div className="text-red-400">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
